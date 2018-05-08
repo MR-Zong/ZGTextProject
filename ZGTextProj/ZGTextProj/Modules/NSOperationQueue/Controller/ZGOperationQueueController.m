da9d85d6c879e6b638217de4e411bf0724e8b8d0
ZongGenXu <540682674@qq.com>
2018-05-08 18:12:54 +0800

    NSOperationQueue

--- ZGTextProj/ZGTextProj/Modules/NSOperationQueue/Controller/ZGOperationQueueController.m
+++ ZGTextProj/ZGTextProj/Modules/NSOperationQueue/Controller/ZGOperationQueueController.m
@@ -23,14 +23,17 @@
     NSLog(@"navigationBar %f",self.navigationController.navigationBar.bounds.size.height);
     
     // 状态栏(statusbar)
-     ;
 //    NSLog(@"status width - %f", rectStatus.size.width); // 宽度
     NSLog(@"status height  %f",  [[UIApplication sharedApplication] statusBarFrame].size.height);  // 高度
     
     // tabBAr 高度
     
     
-    [self testOperationQueue];
+//    [self testOperationQueue];
+    
+    // NSOperationQueue测试：A任务完成后，等待状态的B任务是否会自动执行
+    // 测试结果：NSOperationQueue相当完美！以后自己需要队列管理，都用NSOperationQueue
+    [self testWaitAndContinue2];
 }
 
 - (void)testOperation
@@ -61,6 +64,67 @@
     
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
     [queue addOperation:op1];
+}
+// NSOperationQueue测试：A任务完成后，等待状态的B任务是否会自动执行
+- (void)testWaitAndContinue
+{
+    NSBlockOperation *bopA = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"bopA start");
+        NSLog(@"thread %@",[NSThread currentThread]);
+        sleep(15);
+        NSLog(@"bopA end");
+    }];
+
+
+    NSBlockOperation *bopB = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"bopB start");
+        NSLog(@"thread %@",[NSThread currentThread]);
+        NSLog(@"bopB end");
+    }];
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    queue.maxConcurrentOperationCount = 1;
+    [queue addOperation:bopA];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"add bopB");
+        [queue addOperation:bopB];
+    });
+}
+
+// NSOperationQueue测试：A,B任务其中任何一个任务完成后，等待状态的C任务是否会自动执行
+- (void)testWaitAndContinue2
+{
+    NSBlockOperation *bopA = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"bopA start");
+        NSLog(@"thread %@",[NSThread currentThread]);
+        sleep(15);
+        NSLog(@"bopA end");
+    }];
+    
+    
+    NSBlockOperation *bopB = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"bopB start");
+        NSLog(@"thread %@",[NSThread currentThread]);
+        sleep(25);
+        NSLog(@"bopB end");
+    }];
+
+    NSBlockOperation *bopC = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"bopC start");
+        NSLog(@"thread %@",[NSThread currentThread]);
+        NSLog(@"bopC end");
+    }];
+
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    queue.maxConcurrentOperationCount = 2;
+    [queue addOperation:bopA];
+    [queue addOperation:bopB];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"add bopC");
+        [queue addOperation:bopC];
+    });
 }
 
 
