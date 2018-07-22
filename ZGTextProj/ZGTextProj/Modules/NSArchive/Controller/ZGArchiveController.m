b449139e27163c78aab70c31f83ddae5b87e43c5
ZongGenXu <540682674@qq.com>
2018-07-22 11:03:46 +0800

    archive

--- ZGTextProj/ZGTextProj/Modules/NSArchive/Controller/ZGArchiveController.m
+++ ZGTextProj/ZGTextProj/Modules/NSArchive/Controller/ZGArchiveController.m
@@ -8,6 +8,7 @@
 
 #import "ZGArchiveController.h"
 #import "ZGArchiveModelA.h"
+#import "ZGArchiveModelB.h"
 
 @interface ZGArchiveController ()
 
@@ -22,7 +23,8 @@
     self.title = @"archive";
     self.view.backgroundColor = [UIColor whiteColor];
     
-    [self testArchive];
+//    [self testArchive];
+    [self testArchiveMoreFile];
 }
 
 - (void)testArchive
@@ -43,6 +45,46 @@
     
     ZGArchiveModelA *b = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
     NSLog(@"name %@,age %zd",b.name,b.age);
+}
+
+- (void)testArchiveMoreFile
+{
+    ZGArchiveModelA *a = [[ZGArchiveModelA alloc] init];
+    a.name = @"zong";
+    a.age = 28;
+    a.sex = @"男";
+    a.isMan = YES;
+    
+    ZGArchiveModelB *b = [[ZGArchiveModelB alloc] init];
+    b.name = @"gen";
+    b.age = 30;
+    b.sex = @"女";
+    b.isMan = NO;
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentFilePath = paths.firstObject;
+    NSLog(@"documentPath %@",documentFilePath);
+    NSString *filePath = [documentFilePath stringByAppendingPathComponent:@"moreData"];
+    
+    
+    NSMutableData *mD = [[NSMutableData alloc] init];
+    NSKeyedArchiver *ar = [[NSKeyedArchiver alloc] initForWritingWithMutableData:mD];
+    [ar encodeObject:a forKey:@"zzz"];
+    [ar encodeObject:b forKey:@"ggg"];
+    [ar finishEncoding];
+    
+    [mD writeToFile:filePath atomically:YES];
+    
+    
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    NSKeyedUnarchiver *uAr = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
+    
+    ZGArchiveModelA *aa = [uAr decodeObjectForKey:@"zzz"];
+    ZGArchiveModelB *bb = [uAr decodeObjectForKey:@"ggg"];
+    [uAr finishDecoding];
+    
+    NSLog(@"aa.name %@,age %zd",aa.name,aa.age);
+    NSLog(@"bb.name %@,age %zd",bb.name,bb.age);
 }
 
 
---
+++ ZGTextProj/ZGTextProj/Modules/NSArchive/Model/ZGArchiveModelB.m
@@ -0,0 +1,32 @@
+//
+//  ZGArchiveModelB.m
+//  ZGTextProj
+//
+//  Created by 徐宗根 on 2018/7/22.
+//  Copyright © 2018年 XuZonggen. All rights reserved.
+//
+
+#import "ZGArchiveModelB.h"
+
+@implementation ZGArchiveModelB
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super init]) {
+        _name = [aDecoder decodeObjectForKey:@"ddd"];
+        _age = [aDecoder decodeIntegerForKey:@"age"];
+        _sex = [aDecoder decodeObjectForKey:@"sex"];
+        _isMan = [aDecoder decodeBoolForKey:@"isMan"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:_name forKey:@"ddd"];
+    [aCoder encodeInteger:_age forKey:@"age"];
+    [aCoder encodeObject:_sex forKey:@"sex"];
+    [aCoder encodeBool:_isMan forKey:@"isMan"];
+}
+
+@end
